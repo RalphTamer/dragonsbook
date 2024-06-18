f72141b9472ba9a4d89dea58411af6fb9ca0b77d
ralf tamer <ralf.tamer@gmail.com>
2024-06-19 02:40:46 +0300

    patch

--- src/app/_components/LeaderboardFilter.tsx
+++ src/app/_components/LeaderboardFilter.tsx
@@ -4,6 +4,7 @@ import { getBgColor } from "../_services/leaderboard.service";
 import BottomSlideModal from "./UI/BottomSlideModal";
 import SVGIcon from "./UI/SVGIcon";
 import { useState } from "react";
+import Image from "next/image";
 
 type Props = {
   selectedFilterValue: "FIRE" | "WATER" | "WIND" | "EARTH" | "ALL";
@@ -17,74 +18,107 @@ const LeaderboardFilter = (props: Props) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { selectedFilterValue } = props;
   return (
-    <div
-      className="mb-4 flex justify-between py-4"
-      style={{ fontSize: 13, borderBottom: "1px solid #ccc" }}
-    >
-      <div className=" space-x-1">
-        <div className="flex  items-center space-x-1">
-          <div
-            className="  "
-            style={
-              {
-                // transform: "scale(1, -1)",
-              }
-            }
-          >
-            <SVGIcon className="flex " name="antenna-bars" size={26} />
-          </div>
-          <div
-            className="flex items-center space-x-1"
-            onClick={() => {
-              setModalIsVisible(true);
-            }}
-          >
-            <div>filter by : </div>
-            <div>{humanize(selectedFilterValue)}</div>
-          </div>
-          <BottomSlideModal
-            isOpen={modalIsVisible}
-            onClose={() => {
-              setModalIsVisible(false);
-            }}
-          >
-            <div className="container mb-4 space-y-2">
-              <div className="text-center text-[16px] font-bold uppercase">
-                Filter By
-              </div>
-              {isLoading == false ? (
-                selectModalItems.map((item) => {
-                  return (
-                    <div
-                      key={item}
-                      style={{
-                        background:
-                          item === selectedFilterValue
-                            ? getBgColor(item)
-                            : "#8b8b8b",
-                        borderRadius: "12px",
-                      }}
-                      className="  py-2 text-center text-[18px] text-white"
-                      onClick={async () => {
-                        setModalIsVisible(false);
-                        setIsLoading(true);
-                        props.onChangeModalValue(item);
-
-                        setIsLoading(false);
-                      }}
-                    >
-                      {humanize(item)}
-                    </div>
-                  );
-                })
-              ) : (
-                <div>loading...</div>
-              )}
+    <>
+      <div
+        className="mb-4 flex items-center justify-between py-4"
+        style={{ fontSize: 13, borderBottom: "1px solid #ccc" }}
+      >
+        <div className=" space-x-1">
+          <div className="flex  items-center space-x-1">
+            <div>
+              <SVGIcon className="flex " name="antenna-bars" size={26} />
             </div>
-          </BottomSlideModal>
+            <div
+              className="flex items-center space-x-1"
+              onClick={() => {
+                setModalIsVisible(true);
+              }}
+            >
+              <div>Filter by : </div>
+              <div>{humanize(selectedFilterValue)}</div>
+            </div>
+          </div>
         </div>
+        {selectedFilterValue === "ALL" ? (
+          <div className="flex items-center space-x-2">
+            {["wind", "fire", "earth", "water"].map((element) => {
+              return (
+                <Image
+                  key={element}
+                  src={`/icons/${element}-badge.png`}
+                  alt="image"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  priority
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    //
+                    maxWidth: 20,
+                  }}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <Image
+            src={`/icons/${selectedFilterValue.toLowerCase()}-badge.png`}
+            alt="image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            priority
+            style={{
+              width: "100%",
+              height: "100%",
+              //
+              maxWidth: 20,
+            }}
+          />
+        )}
       </div>
-    </div>
+      <BottomSlideModal
+        isOpen={modalIsVisible}
+        onClose={() => {
+          setModalIsVisible(false);
+        }}
+      >
+        <div className="container mb-4 space-y-2">
+          <div className="text-center text-[16px] font-bold uppercase">
+            Filter By
+          </div>
+          {isLoading == false ? (
+            selectModalItems.map((item) => {
+              return (
+                <div
+                  key={item}
+                  style={{
+                    background:
+                      item === selectedFilterValue
+                        ? getBgColor(item)
+                        : "#8b8b8b",
+                    borderRadius: "12px",
+                  }}
+                  className="  py-2 text-center text-[18px] text-white"
+                  onClick={async () => {
+                    setModalIsVisible(false);
+                    setIsLoading(true);
+                    props.onChangeModalValue(item);
+
+                    setIsLoading(false);
+                  }}
+                >
+                  {humanize(item)}
+                </div>
+              );
+            })
+          ) : (
+            <div>loading...</div>
+          )}
+        </div>
+      </BottomSlideModal>
+    </>
   );
 };
 
